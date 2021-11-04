a0580dd3e1cb7d583dc5e1d2d87dc9836dd7784d
Anderson Rodriguez <andiirc@gmail.com>
2021-11-03 22:15:32 -0500

    create mixin and refactor css components itemcharacter and detailcharacter

--- src/components/DetailCharacter.vue
+++ src/components/DetailCharacter.vue
@@ -100,10 +100,6 @@ export default {
     border-radius: 50%;
     transform: translateY(-40%);
     box-shadow: 0px 5px 50px 0px rgb(43, 227, 252), 0px 0px 0px 7px rgb(3, 152, 172);
-    @media screen and (max-width: 576px) {
-      width: 120px;
-      height: 120px;
-    }
     img {
       display: block;
       width: 100%;
@@ -131,61 +127,31 @@ export default {
         margin: 13px auto;
       }
       .led.active.alive {
-        background-color: #4daa00;
-        box-shadow: 0px 0px 10px 5px rgba(30, 255, 0, 0.4);
-        border: 3px solid rgb(0, 120, 20);
+        @include led-color(#4daa00, rgba(30, 255, 0, 0.4), rgb(0, 120, 20));
         .hilight {
-          width: 4px;
-          height: 4px;
-          border-radius: 50%;
-          position: absolute;
-          background-color: rgba(255,255,255,0.5);
-          top: 7px;
-          left: 7px;
-          box-shadow: 3px 4px 7px 9px rgba(0, 180, 30, 0.6);
+          @include let-wrap-hilight(rgba(0, 180, 30, 0.6));
         }
       }
       .led.active.alive .hilight {
-        background-color: rgba(255,255,255,1);
-        box-shadow: 4px 4px 6px 7px rgb(21, 255, 0);
+        @include led-hilight(rgb(21, 255, 0));
       }
       .led.active.dead {
-        background-color: #AA0000;
-        box-shadow: 0px 0px 10px 5px rgba(255, 0, 0, 0.4);
-        border: 3px solid rgba(120, 0, 0, 1);
+        @include led-color(#AA0000, rgba(255, 0, 0, 0.4), rgba(120, 0, 0, 1));
         .hilight {
-          width: 4px;
-          height: 4px;
-          border-radius: 50%;
-          position: absolute;
-          background-color: rgba(255,255,255,0.5);
-          top: 7px;
-          left: 7px;
-          box-shadow: 3px 4px 7px 9px rgba(180, 0, 0, 0.6);
+          @include let-wrap-hilight(rgba(180, 0, 0, 0.6));
         }
       }
       .led.active.dead .hilight {
-        background-color: rgba(255,255,255,1);
-        box-shadow: 4px 4px 6px 7px rgba(255, 0, 0, 1);
+        @include led-hilight(rgba(255, 0, 0, 1));
       }
       .led.active.default {
-        background-color: #006caa;
-        box-shadow: 0px 0px 10px 5px rgba(0, 47, 255, 0.4);
-        border: 3px solid rgb(0, 12, 120);
+        @include led-color(#006caa, rgba(0, 47, 255, 0.4), rgb(0, 12, 120));
         .hilight {
-          width: 4px;
-          height: 4px;
-          border-radius: 50%;
-          position: absolute;
-          background-color: rgba(255,255,255,0.5);
-          top: 7px;
-          left: 7px;
-          box-shadow: 3px 4px 7px 9px rgba(0, 3, 180, 0.6);
+          @include let-wrap-hilight(rgba(0, 3, 180, 0.6));
         }
       }
       .led.active.default .hilight {
-        background-color: rgba(255,255,255,1);
-        box-shadow: 4px 4px 6px 7px rgb(47, 0, 255);
+        @include led-hilight(rgb(47, 0, 255));
       }
     }
     &__name{
--- src/components/ItemCharacter.vue
+++ src/components/ItemCharacter.vue
@@ -57,12 +57,7 @@ export default {
             }
         }
         h3{
-            font-family: 'Schoolbell', serif;
-            color: $text-primary;
-            text-shadow: 0.02em 0.02em 0.02em #02b1c8,
-                 -0.02em -0.02em 0.02em #02b1c8,
-                 0 0 0.2em #02b1c8;
-            animation: textShadow 2s linear infinite alternate;
+          @include animated-title($text-primary, #02b1c8);
         }
 
     }
@@ -79,84 +74,33 @@ export default {
                     width: 10px;
                     height: 10px;
                     border-radius: 50%;
-                    margin-top: 8px;
+                    margin-top: 6px;
                     margin-right: 12px;
                 }
             }
             .alive {
-                width: 10px;
-                height: 10px;
-                border-radius: 50%;
-                background: forestgreen;
-                display: inline-block;
-                position: relative;
-                box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.2) inset,
-                0 2px 0px rgba(255, 255, 255, 0.3), 0 -1px 0px rgba(0, 0, 0, 0.3);
-                -webkit-animation: blinkgreen 0.5s infinite;
-                -moz-animation: blinkgreen 0.5s infinite;
-                -ms-animation: blinkgreen 0.5s infinite;
-                -o-animation: blinkgreen 0.5s infinite;
-                animation: blinkgreen 0.5s infinite;
+                @include led-blink;
+                @include animation-blink(alive,rgb(8, 194, 54),rgb(6, 116, 42),
+                  rgba(0, 255, 0, 0.589),rgb(111, 241, 132));
                 &:before {
-                    position: absolute;
-                    content: '';
-                    top: 2px;
-                    left: 3px;
-                    width: 3px;
-                    height: 3px;
-                    background: rgba(255, 255, 255, 0.5);
-                    border-radius: 50%;
+                  @include led-wrap;
                  }
             }
             .dead {
-                width: 10px;
-                height: 10px;
-                border-radius: 50%;
-                background: #F00;
-                display: inline-block;
-                position: relative;
-                box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.2) inset,
-                0 2px 0px rgba(255, 255, 255, 0.3), 0 -1px 0px rgba(0, 0, 0, 0.3);
-                -webkit-animation: blinkred 0.5s infinite;
-                -moz-animation: blinkred 0.5s infinite;
-                -ms-animation: blinkred 0.5s infinite;
-                -o-animation: blinkred 0.5s infinite;
-                animation: blinkred 0.5s infinite;
+                @include led-blink;
+                @include animation-blink(dead, #F00, #A00,
+                  rgba(255, 0, 0, 0.5), rgb(231, 108, 108));
                 &:before {
-                    position: absolute;
-                    content: '';
-                    top: 2px;
-                    left: 3px;
-                    width: 3px;
-                    height: 3px;
-                    background: rgba(255, 255, 255, 0.5);
-                    border-radius: 50%;
+                  @include led-wrap;
                 }
 
             }
             .default {
-                width: 10px;
-                height: 10px;
-                border-radius: 50%;
-                background: #24E0FF;
-                display: inline-block;
-                position: relative;
-                box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.2) inset,
-                0 2px 0px rgba(255, 255, 255, 0.3), 0 -1px 0px rgba(0, 0, 0, 0.3);
-                -webkit-animation: blinkblue 0.5s infinite;
-                -moz-animation: blinkblue 0.5s infinite;
-                -ms-animation: blinkblue 0.5s infinite;
-                -o-animation: blinkblue 0.5s infinite;
-                animation: blinkblue 0.5s infinite;
+               @include led-blink;
+               @include animation-blink(unknown, rgb(62, 81, 253), rgb(7, 10, 155),
+                  rgba(41, 2, 148, 0.5), rgb(131, 143, 253));
                 &:before {
-                    position: absolute;
-                    content: '';
-                    top: 2px;
-                    left: 3px;
-                    width: 3px;
-                    height: 3px;
-                    background: rgba(255, 255, 255, 0.5);
-                    border-radius: 50%;
+                  @include led-wrap;
                 }
             }
         }
@@ -179,43 +123,7 @@ export default {
     &__name{
         display: flex;
         justify-content: center;
-        font-family: 'Schoolbell', serif;
-        color: #02b1c8;
-        text-shadow: 0.02em 0.02em 0.02em $text-quaternary,
-                 -0.02em -0.02em 0.02em $text-quaternary,
-                 0 0 0.2em $text-quaternary;
-        animation: textShadow 2s linear infinite alternate;
+        @include animated-title(#02b1c8, $text-quaternary);
     }
-
-@-webkit-keyframes blinkblue {
-    from { background-color: rgb(62, 81, 253); }
-    50% {
-          background-color: rgb(7, 10, 155);
-          box-shadow: rgba(0, 0, 0, 0.2) 0 -1px 7px 1px,
-          inset #441313 0 -1px 9px, rgba(41, 2, 148, 0.5) 0 2px 0;
-        }
-    to { background-color: rgb(131, 143, 253); }
-}
-
-@-webkit-keyframes blinkred {
-    from { background-color: #F00; }
-    50% {
-          background-color: #A00;
-          box-shadow: rgba(0, 0, 0, 0.2) 0 -1px 7px 1px,
-          inset #441313 0 -1px 9px, rgba(255, 0, 0, 0.5) 0 2px 0;
-        }
-    to { background-color: rgb(231, 108, 108); }
-}
-
-@-webkit-keyframes blinkgreen {
-    from { background-color: rgb(8, 194, 54); }
-    50% {
-          background-color: rgb(6, 116, 42);
-          box-shadow: rgba(0, 0, 0, 0.2) 0 -1px 7px 1px,
-          inset #441313 0 -1px 9px, rgba(0, 255, 0, 0.589) 0 2px 0;
-        }
-    to { background-color: rgb(111, 241, 132); }
-}
-
 }
 </style>
